bf7ac8ae6b0bfc9e691c1114db67f9bfa6be1383
Mladen Konkolj <mladenkonkolj@gmail.com>
2022-01-18 12:41:05 +0100

    postavljeni edit, delete i add modali

--- script/script.js
+++ script/script.js
@@ -1,34 +1,61 @@
-// DELETE MODAL BTNS
+// DELETE MODAL
 const deleteBtn = document.querySelector("#deleteBtn");
 const deleteModal = document.querySelector("#deleteModal");
-const deleteYes = document.querySelector(".deleted-yes");
-const deleteNo = document.querySelector(".deleted-no");
+const deleteYes = document.querySelector("#deleteYes");
+const deleteNo = document.querySelector("#deleteNo");
 
-// ADD USER MODAL BTN
-const AddUserBtn = document.querySelector(".add-user-btn");
-const AddUserModal = document.querySelector("#addUserModal");
-const AddUserCancel = document.querySelector("#addUserCancel");
-const AddUserAdd = document.querySelector("#addUserAdd");
+// ADD USER MODAL
+const addUserBtn = document.querySelector("#addUserBtn");
+const addUserModal = document.querySelector("#addUserModal");
+const addUserCancel = document.querySelector("#addUserCancel");
+const addUserAdd = document.querySelector("#addUserAdd");
 
+//EDIT USER
+const editBtn = document.querySelector("#editBtn");
+const editUserCancel = document.querySelector("#editUserCancel");
+const editUserSave = document.querySelector("#editUserSave");
+const editUserModal = document.querySelector("#editUserModal");
+
+//OTHER
+const blackAlpha = document.querySelector("#blackAlpha");
 
 ////////////////////////////////////////////////////
 // EVENTS FOR DELETE MODAL
-deleteBtn.addEventListener("click", showDeleteModal);
-deleteYes.addEventListener("click", showDeleteModal);
-deleteNo.addEventListener("click", showDeleteModal);
+deleteBtn.addEventListener("click", toggleDeleteModal);
+deleteYes.addEventListener("click", toggleDeleteModal);
+deleteNo.addEventListener("click", toggleDeleteModal);
 
 // EVENTS FOR ADD USER MODAL
-AddUserBtn.addEventListener("click", showAddUserModal);
-AddUserCancel.addEventListener("click", showAddUserModal);
-AddUserAdd.addEventListener("click", showAddUserModal);
+addUserBtn.addEventListener("click", toggleNewUserModal);
+addUserCancel.addEventListener("click", toggleNewUserModal);
+addUserAdd.addEventListener("click", toggleNewUserModal);
 
+// EVENTS FOR EDIT USER MODAL
+editBtn.addEventListener("click", toggleEditUserModal);
+editUserCancel.addEventListener("click", toggleEditUserModal);
+editUserSave.addEventListener("click", toggleEditUserModal);
 
 /////////////////////////////////////////////////////
 
-function showDeleteModal() {
+function toggleDeleteModal() {
     deleteModal.classList.toggle("show");
+    blackAlpha.classList.toggle("show");
 };
 
-function showAddUserModal() {
-    AddUserModal.classList.toggle("show");
+function toggleNewUserModal() {
+    addUserModal.classList.toggle("show");
+    blackAlpha.classList.toggle("show");
 };
+
+function toggleEditUserModal() {
+    editUserModal.classList.toggle("show");
+    blackAlpha.classList.toggle("show");
+};
+
+//BLACK ALPHA CLOSE MODAL
+blackAlpha.addEventListener("click", () => {
+    deleteModal.classList.remove("show");
+    addUserModal.classList.remove("show");
+    editUserModal.classList.remove("show");
+    blackAlpha.classList.remove("show");
+});
